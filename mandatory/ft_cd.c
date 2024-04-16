829aea52a3a95d9ed09ceb478b4f25739e3c512b
nmihaile <nmihaile@2-E-3.42heilbronn.de>
2024-04-16 12:39:32 +0200

    changes err msg for cd - when OLDPWD is not set

--- mandatory/ft_cd.c
+++ mandatory/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 00:41:52 by aalatzas          #+#    #+#             */
-/*   Updated: 2024/04/15 22:12:06 by nmihaile         ###   ########.fr       */
+/*   Updated: 2024/04/16 12:36:38 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,7 +90,7 @@ static int	ft_cd2(t_cmd *cmd, t_ms *ms, char *old_cwd, char *current_cwd)
 	&& ft_strncmp(cmd->tokens[0]->next->str, "-\0", 2) == 0)
 	{
 		if (ft_strlen(old_cwd) == 0)
-			return (printf("\n"), 1);
+			return (ft_error("cd", "OLDPWD not set", NULL), 1);
 		ft_get_env_value(ms, current_cwd, "PWD");
 		if (ft_chdir(ms, old_cwd) != 0)
 			return (ft_double_perror("cd", old_cwd), 1);
